3666f5ea7ec93d7a0cc0cf1438df426bf6910eaf
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-12-14 13:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function validEmail(str) {
   //your JS code here.
+let validRegex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+	if(str.match(validRegex)){
+		return true;
+	}else{
+		return false;
+	}
 }
-
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
